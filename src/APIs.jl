59351b66595b8f3ca16f7fcef61b30a0eaa32265
JinraeKim <kjl950403@gmail.com>
2021-03-12 11:07:32 +0900

    Add docs

--- src/APIs.jl
+++ src/APIs.jl
@@ -1,4 +1,9 @@
 ## ODEProblem
+"""
+    ODEProblem(env::AbstractEnv, f, x0, tspan; kwargs...)
+
+An API for DifferentialEquations.ODEProblem(f, x0, tspan; kwargs...).
+"""
 function DifferentialEquations.ODEProblem(env::AbstractEnv, f, x0, tspan; kwargs...)
     warn_is_registered(env)
     _x0 = raw(env, x0)
@@ -8,6 +13,11 @@ end
 
 ## initial condition
 # automatic completion of initial condition
+"""
+    initial_condition(env::AbstractEnv)
+
+Automatic completion of readable initial_condition if methods of its sub-environments are given.
+"""
 function initial_condition(env::AbstractEnv)
     env_names = names(env)
     values = env_names |> Map(name -> initial_condition(getfield(env, name)))
@@ -17,9 +27,14 @@ end
 
 ## register env
 """
+    @reg_env(env, x0)
+
+Register given environment.
+`x0` is an example of readable initial_condition for `env` so that `NestedEnvironments` infers the information of `env`.
+
 # Notes
 - Must be used in the global scope.
-- x0 can be any dummy collection such as NamedTuple-valued initial value.
+- `x0` can be any dummy NamedTuple-collection.
 """
 macro reg_env(env, x0)
     ex = quote
@@ -50,6 +65,11 @@ end
 
 ## macros
 # macro for transformation from readable to raw
+"""
+    @raw(env, x)
+
+A macro that transforms a readable (NamedTuple) value to raw (flattened array) value.
+"""
 macro raw(env, x)
     ex = quote
         if typeof($(env)) <: AbstractEnv
@@ -61,6 +81,11 @@ macro raw(env, x)
     esc(ex)
 end
 # auto-completion
+"""
+    @raw(x)
+
+A macro that transforms a readable (NamedTuple) value to raw (flattened array) value by corresponding an appropriate environment from the registered environments.
+"""
 macro raw(x)
     ex = quote
         local env_x_cands = zip(NestedEnvironments.__REGISTERED_ENVS.__envs, NestedEnvironments.__REGISTERED_ENVS.__xs) |> Filter(env_x -> size(env_x[2]) == size($(x))) |> collect
@@ -77,8 +102,9 @@ end
 
 # macro for transformation from raw to readable
 """
-# Notes
-Use it after @reg_env
+    @readable(env, _x)
+
+A macro that transforms a raw (flattened array) value to readable (NamedTuple) value.
 """
 macro readable(env, _x)
     ex = quote
@@ -91,6 +117,11 @@ macro readable(env, _x)
     esc(ex)
 end
 # auto-completion
+"""
+    @readable(x)
+
+A macro that transforms a raw (flattened array) value to readable (NamedTuple) value by corresponding an appropriate environment from the registered environments.
+"""
 macro readable(_x)
     ex = quote
         local env_x_cands = zip(NestedEnvironments.__REGISTERED_ENVS.__envs, NestedEnvironments.__REGISTERED_ENVS.__xs) |> Filter(env_x -> NestedEnvironments.flatten_length(env_x[2]) == NestedEnvironments.flatten_length($(_x))) |> collect
