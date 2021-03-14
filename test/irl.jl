7a8b2e22d68ff36e7e2a4819f5460674a16070d5
JinraeKim <kjl950403@gmail.com>
2021-03-14 09:55:09 +0900

    wip

--- test/irl.jl
+++ test/irl.jl
@@ -12,18 +12,27 @@ function dynamics(env::Env)
         x = x_nt.iaqc
         ∫r = x_nt.∫r
         u = 0.0
-        NestedEnvironments.ẋ(env.iaqc, x, t, u)
-        (; env = , ∫r = NestedEnvironments.r(env.iaqc, x, u))
+        dx = NestedEnvironments.ẋ(env.iaqc, x, t, u)
+        (; iaqc = dx, ∫r = NestedEnvironments.r(env.iaqc, x, u))
     end
 end
 
-initial_condition(env::InputAffineQuadraticCostEnv) = 2 * (rand(2) .- 0.5)
+NestedEnvironments.initial_condition(env::InputAffineQuadraticCostEnv) = 2 * (rand(2) .- 0.5)
 
-
-function test()
+function make_env()
     iaqc = InputAffineQuadraticCostEnv()
     ∫r = BaseEnv()  # 0.0
     env = Env(iaqc, ∫r)
+    env
+end
+
+# register
+__env = make_env()
+__x0 = initial_condition(__env)
+@reg_env __env __x0
+
+function test()
+    env = make_env()
     x0 = initial_condition(env)
     t0 = 0.0
     tf = 10.0
@@ -31,5 +40,9 @@ function test()
     ts = t0:Δt:tf
     tspan = (t0, tf)
     prob = ODEProblem(env, dynamics(env), x0, tspan)
-    sol = solve(prob, Tsit5(), saveat=ts)
+    saved_values = SavedValues(Float64, NamedTuple)
+    cb_save = SavingCallback((_x, t, integrator) -> (; x = (@readable _x).iaqc), saved_values, saveat=ts)
+    cb = CallbackSet(cb_save)
+    _ = solve(prob, Tsit5(), callback=cb)
+    saved_values.saveval
 end
