ffc4ccd382466695fd1d9c6676250b97c2a54db2
Jinrae Kim <kjl950403@gmail.com>
2021-03-17 11:04:08 +0900

    Fix a bug; to support parameter (#13)

--- src/APIs.jl
+++ src/APIs.jl
@@ -10,6 +10,12 @@ function DifferentialEquations.ODEProblem(env::AbstractEnv, f, x0, tspan; kwargs
     _f(_x, p, t) = raw(env, f(readable(env, _x), p, t))
     ODEProblem(_f, _x0, tspan; kwargs...)
 end
+function DifferentialEquations.ODEProblem(env::AbstractEnv, f, x0, tspan, p; kwargs...)
+    warn_is_registered(env)
+    _x0 = raw(env, x0)
+    _f(_x, p, t) = raw(env, f(readable(env, _x), p, t))
+    ODEProblem(_f, _x0, tspan, p; kwargs...)
+end
 
 ## initial condition
 # automatic completion of initial condition
